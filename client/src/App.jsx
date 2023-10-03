0460bff11cba22a90d05e2f64070ceafdcadc85d
cool-sabbir <sabbirmusfique1@gmail.com>
2023-10-03 14:00:03 +0600

    faefa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <div>
       Hello from App.jsx
+      <h1>Hello World!</h1>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
